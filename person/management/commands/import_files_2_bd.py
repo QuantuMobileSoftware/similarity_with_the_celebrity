61fc41dde2ca46f1ff2c0f79d45a0ecbdea95a2c
yevheniia <y.smyrnova@quantumobile.com>
2017-07-24 16:22:25 +0300

    fix issue with load information on db

--- person/management/commands/import_files_2_bd.py
+++ person/management/commands/import_files_2_bd.py
@@ -10,11 +10,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         root_dir = './Photos'
         for dir in os.listdir(root_dir):
-                person_id = Person.objects.create(name=dir)
-                for file in os.listdir(os.path.join(root_dir, dir)):
-                    if file.endswith(('jpg', 'png')):
-                        img = face_recognition.api.load_image_file(os.path.join(root_dir, dir, file))
-                        face_encoding = get_face_encodings(img)
-                        if len(face_encoding) == 1:
-                            face_encoding = str(face_encoding[0].tolist())[1:-1]
-                            Face.objects.create(person=person_id, encoding=face_encoding)
+            person_id = Person.objects.create(name=dir)
+            for file in os.listdir(os.path.join(root_dir, dir)):
+                if file.endswith(('jpg', 'png')):
+                    img = face_recognition.api.load_image_file(os.path.join(root_dir, dir, file))
+                    face_encoding = get_face_encodings(img)
+                    if len(face_encoding) == 1:
+                        face_encoding = str(face_encoding[0].tolist())[1:-1]
+                        Face.objects.create(person=person_id, encoding=face_encoding)
