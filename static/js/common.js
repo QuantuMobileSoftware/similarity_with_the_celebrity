c7d880e7fca143944cd343951a9de13574a92360
Yevheniia Smyrnova <YevheniiaSmyrnova@gmail.com>
2020-09-01 16:52:26 +0300

    12994: fix issue with video

--- static/js/common.js
+++ static/js/common.js
@@ -38,7 +38,7 @@ $(window.document).ready(function () {
 
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
-      video.src = window.URL.createObjectURL(stream);
+      video.srcObject = stream;
       video.play();
       video.onloadedmetadata = function() {
       videoWidth = this.videoWidth;
